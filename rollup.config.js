53fc60964cdf61993d33be57fcc6fe0b0e7e1657
Markus Felten <Markus.Felten@gmx.de>
2019-07-15 16:09:48 +0200

    fix: add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,99 @@
+import copy from "rollup-plugin-copy";
+import replace from "rollup-plugin-replace";
+import vue from "rollup-plugin-vue";
+import css from "rollup-plugin-css-only";
+import json from "rollup-plugin-json";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import nodeGlobals from "rollup-plugin-node-globals";
+import pkg, { config } from "./package.json";
+import history from "connect-history-api-fallback";
+import proxy from "http-proxy-middleware";
+import express from "express";
+import { create as browserSyncFactory } from "browser-sync";
+
+const isProduction = !process.env.ROLLUP_WATCH;
+const dist = "build/dist";
+
+const globals = { vue: "Vue" };
+const external = Object.keys(pkg.dependencies);
+
+const rollupConfig = {
+  input: "src/main.mjs",
+  //external,
+
+  output: {
+    //globals,
+    file: `${dist}/bundle.js`,
+    sourcemap: !isProduction,
+    format: "esm"
+  },
+  /*
+  output: {
+    globals,
+    name: 'bundle',
+    file: `${dist}/bundle.js`,
+    format: 'umd'
+  },
+  */
+  watch: {
+    include: "src/**/*"
+  },
+  plugins: [
+    json({
+      preferConst: true,
+      compact: true
+    }),
+    resolve(),
+    commonjs(),
+    nodeGlobals(),
+    css({ output: `${dist}/bundle.css` }),
+    vue({
+      template: {
+        isProduction,
+        compilerOptions: { preserveWhitespace: false }
+      },
+      css: false
+    }),
+    copy({
+      targets: [
+        { src: "public/index.html", dest: dist },
+        { src: "public/favicon.ico", dest: dist }
+      ]
+    }),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("production")
+    })
+  ]
+};
+
+if (isProduction) {
+  process.env.NODE_ENV = "production";
+} else {
+  function browsersync() {
+    const browserSync = browserSyncFactory();
+    const app = express();
+
+    app.use(
+      config.urlPrefix + config.api,
+      proxy({
+        target: config.proxyTarget,
+        changeOrigin: true,
+        ws: true,
+        logLevel: "debug"
+      })
+    );
+
+    browserSync.init({
+      server: dist,
+      watch: true,
+      middleware: [app , history()]
+    });
+  }
+
+  setTimeout(() => {
+    browsersync();
+  }, 500);
+}
+
+export default rollupConfig;
